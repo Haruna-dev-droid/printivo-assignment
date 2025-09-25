1d9eba8a929414c96ddbd26c90121cec62d9aeaf
John Doe <samailah40@gmail.com>
2025-09-25 18:02:20 +0100

    first commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Products />
+    </>
+  );
+}
+
+export default App;
